51da2b8b5696f765b807313584421672bfc0dcba
Southampton Open Data <opendata@soton.ac.uk>
2013-09-17 08:47:49 +0100

    Added a crude credits page

---
+++ docs/credits.php
@@ -0,0 +1,20 @@
+<?php
+
+$f3->set('page_title', 'Credits');
+
+?>
+
+<table class="credits">
+<tr><td colspan="2"><h3>Programming credits</h3></td></tr>
+<tr><td>Main programmer:</td><td>Ash Smith</td></tr>
+<tr><td>Legacy realtime code:</td><td>Ashley Browning<br>John Isger</td></tr>
+<tr><td>Live times Javascript</td><td>Christopher Gutteridge</td></tr>
+<tr><td>Maps</td><td>Chris Baines</td></tr>
+<tr><td colspan="2"><h3>Data credits</h3></td></tr>
+<tr><td>Live bus times</td><td>Southampton City Council</td></tr>
+<tr><td>Southampton points of interest</td><td><a href="http://dbpedia.org">dbpedia.org</a></td></tr>
+<tr><td>Food/drink establishment data</td><td><a href="http://www.food.gov.uk/">food.gov.uk</a></td></tr>
+<tr><td>Map data</td><td><a href="http://www.openstreetmap.org">openstreetmap.org</a></td></tr>
+</table>
+
+<p>This is a free service provided by the University of Southampton. All data displayed is from other sources and, while we make every effort to ensure that the data is correct, we cannot be held responsible if information displayed is inaccurate. Despite the technological revolution, the only authoratitive source of bus data is still a bus timetable!</p>
